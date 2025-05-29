4ee6360bb9ca559968300e4ca9e45b77b0a80edb
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2025-05-30 03:08:02 +0400

    changed package-lock.json package.json src/App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CommentsList from './componets/CommentsList';
 import TemperatureConverter from './componets/TemperatureConverter';
+import TodoList from './componets/TodoList';
 
 function App() {
   return (
     <div className="App">
       <CommentsList />
       <TemperatureConverter />
+      <TodoList />
     </div>
   );
 }

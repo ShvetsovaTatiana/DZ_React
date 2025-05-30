94f0a7e8bab0e7add22800bcd830d95f4deb9073
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2025-05-30 13:05:32 +0400

    changed package-lock.json package.json src/App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import CommentsList from './componets/CommentsList';
 import TemperatureConverter from './componets/TemperatureConverter';
 import TodoList from './componets/TodoList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './componets/HomePage';
+import AboutPage from './componets/AboutPage';
+
 
 function App() {
   return (
@@ -10,6 +14,12 @@ function App() {
       <CommentsList />
       <TemperatureConverter />
       <TodoList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
